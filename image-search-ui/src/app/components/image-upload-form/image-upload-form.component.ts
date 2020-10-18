4cf3148eb0848d06ec4c4cfbff326ab2a2183958
Yousuf Khan <bitsol@DESKTOP-95CVSDT.localdomain>
2020-10-18 12:02:04 +0500

    feat(image-upload-form): added file selection input validation

--- image-search-ui/src/app/components/image-upload-form/image-upload-form.component.ts
+++ image-search-ui/src/app/components/image-upload-form/image-upload-form.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-image-upload-form',
   templateUrl: './image-upload-form.component.html',
-  styleUrls: ['./image-upload-form.component.scss']
+  styleUrls: ['./image-upload-form.component.scss'],
 })
 export class ImageUploadFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  handleFileChange = (files: FileList) => {
+    const file = files[0];
 
+    const isValid = this.validateFile(file);
+
+    if (isValid) {
+      console.log('valid file');
+    } else {
+      console.log('invalid file');
+    }
+  };
+
+  handleSubmit = () => {
+    console.log('form submitted');
+  };
+
+  validateFile = (file?: File): boolean => {
+    let valid: boolean = true;
+
+    return valid;
+  };
 }
--- image-search-ui/src/app/components/image-upload/image-upload.component.ts
+++ image-search-ui/src/app/components/image-upload/image-upload.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-image-upload',
   templateUrl: './image-upload.component.html',
-  styleUrls: ['./image-upload.component.scss']
+  styleUrls: ['./image-upload.component.scss'],
 })
 export class ImageUploadComponent implements OnInit {
+  @Input() onChange: (files: FileList) => void;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
