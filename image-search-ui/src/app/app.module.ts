9ba8a4ab02282b7297547c6738ce1b62596abc88
Yousuf Khan <bitsol@DESKTOP-95CVSDT.localdomain>
2020-10-18 10:37:45 +0500

    feat(image-uploader): added a image uploaded module

--- image-search-ui/src/app/app.module.ts
+++ image-search-ui/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageUploadComponent } from './components/image-upload/image-upload.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ImageUploadComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
