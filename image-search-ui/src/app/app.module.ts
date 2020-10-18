cdac778e5a5a86e814fb0bc18583464e275657a0
Yousuf Khan <bitsol@DESKTOP-95CVSDT.localdomain>
2020-10-18 10:42:31 +0500

    feat(image-upload-form): added image uploader form

--- image-search-ui/src/app/app.module.ts
+++ image-search-ui/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageUploadComponent } from './components/image-upload/image-upload.component';
+import { ImageUploadFormComponent } from './components/image-upload-form/image-upload-form.component';
 
 @NgModule({
-  declarations: [AppComponent, ImageUploadComponent],
+  declarations: [AppComponent, ImageUploadComponent, ImageUploadFormComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ image-search-ui/src/app/components/image-upload-form/image-upload-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImageUploadFormComponent } from './image-upload-form.component';
+
+describe('ImageUploadFormComponent', () => {
+  let component: ImageUploadFormComponent;
+  let fixture: ComponentFixture<ImageUploadFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ImageUploadFormComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ImageUploadFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
