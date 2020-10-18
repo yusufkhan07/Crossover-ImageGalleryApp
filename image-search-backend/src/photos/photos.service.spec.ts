d9815e89646ced916264de58387e4bf7ca8001ce
Yousuf Khan <bitsol@DESKTOP-95CVSDT.localdomain>
2020-10-18 20:27:27 +0500

    test(photos-service): add sequelize & repository mock

--- image-search-backend/src/photos/photos.service.spec.ts
+++ image-search-backend/src/photos/photos.service.spec.ts
@@ -1,12 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Sequelize } from 'sequelize-typescript';
+
 import { PhotosService } from './photos.service';
+import { Photo } from './models/photo.model';
 
 describe('PhotosService', () => {
   let service: PhotosService;
 
   beforeEach(async () => {
+    const sequelize = new Sequelize({
+      database: 'some_db',
+      dialect: 'sqlite',
+      username: 'root',
+      password: '',
+      storage: ':memory:',
+      logging: false,
+      models: [Photo],
+    });
+
+    await sequelize.sync({
+      force: true,
+    });
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PhotosService],
+      providers: [
+        {
+          provide: `PhotoRepository`,
+          useValue: sequelize.model(Photo.getTableName() as string),
+        },
+        PhotosService,
+      ],
     }).compile();
 
     service = module.get<PhotosService>(PhotosService);
@@ -20,8 +43,10 @@ describe('PhotosService', () => {
     fail();
   });
 
-  it(`should upload photo to S3 when RDS op is successfull`, () => {
-    fail();
+  it(`should upload photo to S3 when RDS op is successfull`, async () => {
+    const created = await service.create({});
+
+    expect(created).toBeInstanceOf(Photo);
   });
 
   it(`should not upload photo to S3 when RDS op fails`, () => {
--- image-search-backend/src/photos/photos.service.ts
+++ image-search-backend/src/photos/photos.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotImplementedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+
+import { Photo } from './models/photo.model';
 
 @Injectable()
-export class PhotosService {}
+export class PhotosService {
+  constructor(
+    @InjectModel(Photo)
+    private photoModel: typeof Photo,
+  ) {}
+
+  private async uploadToS3(file) {
+    throw new NotImplementedException();
+  }
+
+  // inserts record in RDS and upload image to s3.
+  async create(photo): Promise<Photo> {
+    const created = await this.photoModel.create(photo);
+
+    await this.uploadToS3(photo);
+
+    return created;
+  }
+}
