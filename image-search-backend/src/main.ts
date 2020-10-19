e6bda7c5506d7a1666789661387e28be6b560dd4
Yousuf Khan <bitsol@DESKTOP-95CVSDT.localdomain>
2020-10-19 14:28:01 +0500

    feat(api): enable cors

--- image-search-backend/src/main.ts
+++ image-search-backend/src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const options = new DocumentBuilder()
     .setTitle('Image Server')
     .setDescription('The Image Server API description')
