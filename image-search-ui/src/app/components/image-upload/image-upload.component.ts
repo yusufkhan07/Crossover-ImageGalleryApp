f0d0185c7accdab8ba6fad9a9a4d99a41a06bcf8
Yousuf Khan <bitsol@DESKTOP-95CVSDT.localdomain>
2020-10-24 15:05:03 +0500

    feat(upload-form-ui): updated ui of the form

--- image-search-ui/src/app/components/image-upload/image-upload.component.ts
+++ image-search-ui/src/app/components/image-upload/image-upload.component.ts
@@ -8,7 +8,17 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ImageUploadComponent implements OnInit {
   @Input() onChange: (files: FileList) => void;
 
+  fileName: string;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  setSelectedFileName = (files: FileList) => {
+    if (files && files.length > 0) {
+      this.fileName = files[0].name;
+    } else {
+      this.fileName = undefined;
+    }
+  };
 }
