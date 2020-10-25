f6a5275ae101e46e54ee183f66b7ca8883ff84a2
Yousuf Khan <bitsol@DESKTOP-95CVSDT.localdomain>
2020-10-25 15:30:47 +0500

    feat(img-gallery): load images on scroll

--- image-search-ui/src/app/components/image-gallery/image-gallery.component.ts
+++ image-search-ui/src/app/components/image-gallery/image-gallery.component.ts
@@ -8,38 +8,39 @@ import { PhotoSearchService } from '../../services/photo-search.service';
   styleUrls: ['./image-gallery.component.scss'],
 })
 export class ImageGalleryComponent implements OnInit {
-  images: Array<any>;
+  images: Array<{
+    id: number;
+    description: string;
+    url: string;
+    size: number;
+    mimetype: string;
+  }>;
   lastLoadedPage: number;
 
   constructor(private readonly searchService: PhotoSearchService) {}
 
-  ngOnInit(): void {
-    this.images = [
-      {
-        id: 1,
-        description: 'accusamus beatae ad facilis cum similique qui sunt',
-        url: 'https://via.placeholder.com/600/92c952',
-      },
-      {
-        id: 2,
-        description: 'reprehenderit est deserunt velit ipsam',
-        url: 'https://via.placeholder.com/600/771796',
-      },
-      {
-        id: 3,
-        description: 'officia porro iure quia iusto qui ipsa ut modi',
-        url: 'https://via.placeholder.com/600/24f355',
-      },
-      {
-        id: 4,
-        description:
-          'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
-        url: 'https://via.placeholder.com/600/d32776',
-      },
-    ];
+  private loadImages = async () => {
+    const pageToLoad =
+      this.lastLoadedPage === undefined ? 0 : this.lastLoadedPage + 1;
+
+    // load images
+    try {
+      const newImages = await this.searchService.search(pageToLoad);
+
+      this.images = [...(this.images || []), ...newImages];
+    } catch {
+      return alert('error loading images');
+    }
+
+    this.lastLoadedPage = pageToLoad;
+    return this.images;
+  };
+
+  async ngOnInit(): Promise<void> {
+    await this.loadImages();
   }
 
-  onScroll() {
-    console.log('scrolled');
-  }
+  onScroll = async () => {
+    await this.loadImages();
+  };
 }
--- image-search-ui/src/app/services/photo-search.service.ts
+++ image-search-ui/src/app/services/photo-search.service.ts
@@ -6,7 +6,9 @@ import { Injectable } from '@angular/core';
 export class PhotoSearchService {
   constructor() {}
 
-  async search(): Promise<
+  async search(
+    curPage: number
+  ): Promise<
     Array<{
       id: number;
       description: string;
@@ -15,6 +17,92 @@ export class PhotoSearchService {
       mimetype: string;
     }>
   > {
-    return [];
+    // put in real API call
+    return [
+      {
+        id: 1,
+        description: 'accusamus beatae ad facilis cum similique qui sunt',
+        url: 'https://via.placeholder.com/600/92c952',
+      },
+      {
+        id: 2,
+        description: 'reprehenderit est deserunt velit ipsam',
+        url: 'https://via.placeholder.com/600/771796',
+      },
+      {
+        id: 3,
+        description: 'officia porro iure quia iusto qui ipsa ut modi',
+        url: 'https://via.placeholder.com/600/24f355',
+      },
+      {
+        id: 4,
+        description:
+          'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
+        url: 'https://via.placeholder.com/600/d32776',
+      },
+      {
+        id: 1,
+        description: 'accusamus beatae ad facilis cum similique qui sunt',
+        url: 'https://via.placeholder.com/600/92c952',
+      },
+      {
+        id: 2,
+        description: 'reprehenderit est deserunt velit ipsam',
+        url: 'https://via.placeholder.com/600/771796',
+      },
+      {
+        id: 3,
+        description: 'officia porro iure quia iusto qui ipsa ut modi',
+        url: 'https://via.placeholder.com/600/24f355',
+      },
+      {
+        id: 4,
+        description:
+          'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
+        url: 'https://via.placeholder.com/600/d32776',
+      },
+      {
+        id: 1,
+        description: 'accusamus beatae ad facilis cum similique qui sunt',
+        url: 'https://via.placeholder.com/600/92c952',
+      },
+      {
+        id: 2,
+        description: 'reprehenderit est deserunt velit ipsam',
+        url: 'https://via.placeholder.com/600/771796',
+      },
+      {
+        id: 3,
+        description: 'officia porro iure quia iusto qui ipsa ut modi',
+        url: 'https://via.placeholder.com/600/24f355',
+      },
+      {
+        id: 4,
+        description:
+          'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
+        url: 'https://via.placeholder.com/600/d32776',
+      },
+      {
+        id: 1,
+        description: 'accusamus beatae ad facilis cum similique qui sunt',
+        url: 'https://via.placeholder.com/600/92c952',
+      },
+      {
+        id: 2,
+        description: 'reprehenderit est deserunt velit ipsam',
+        url: 'https://via.placeholder.com/600/771796',
+      },
+      {
+        id: 3,
+        description: 'officia porro iure quia iusto qui ipsa ut modi',
+        url: 'https://via.placeholder.com/600/24f355',
+      },
+      {
+        id: 4,
+        description:
+          'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
+        url: 'https://via.placeholder.com/600/d32776',
+      },
+    ] as any;
   }
 }
